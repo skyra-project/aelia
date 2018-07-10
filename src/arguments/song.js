92ed297507062cc37c63e008d84e8b06684841e3
kyraNET <kyradiscord@gmail.com>
2018-07-10 20:33:28 +0200

    src: Smarter URL parser for song parameter

--- src/arguments/song.js
+++ src/arguments/song.js
@@ -1,5 +1,4 @@
 const { Argument } = require('klasa');
-const { parse } = require('url');
 
 module.exports = class extends Argument {
 
@@ -7,12 +6,14 @@ module.exports = class extends Argument {
 		if (!msg.guild) return null;
 
 		arg = arg.replace(/<(.+)>/g, '$1');
-		if (this.isLink(arg))
-			return this.client.lavalink.resolveTracks(arg);
+		const parsedURL = this.parseURL(arg);
+		if (parsedURL) {
+			const tracks = await this.client.lavalink.resolveTracks(arg);
+			if (!tracks.length) throw `Could not find any results for \`${arg}\``;
+			return parsedURL.playlist ? tracks : tracks[0];
+		}
 
-		const searchSoundcloud = 'sc' in msg.flags || 'soundcloud' in msg.flags;
-
-		if (!searchSoundcloud) {
+		if (!(('sc' in msg.flags) || ('soundcloud' in msg.flags))) {
 			const tracks = await this.client.lavalink.resolveTracks(`ytsearch: ${arg}`);
 			if (!tracks.length) tracks.push(...await this.client.lavalink.resolveTracks(`scsearch: ${arg}`));
 			if (!tracks.length) throw `Could not find any results for \`${arg}\``;
@@ -24,9 +25,15 @@ module.exports = class extends Argument {
 		return tracks[0];
 	}
 
-	isLink(url) {
-		const res = parse(url);
-		return res.protocol && res.hostname && (res.protocol === 'https:' || res.protocol === 'http:');
+	parseURL(url) {
+		try {
+			const parsed = new URL(url);
+			return parsed.protocol && parsed.hostname && (parsed.protocol === 'https:' || parsed.protocol === 'http:')
+				? { url: parsed.href, playlist: parsed.searchParams.has('list') }
+				: null;
+		} catch (error) {
+			return null;
+		}
 	}
 
 };
