f1afa3e815ef468bab51e4a0a380565b781577e7
kyraNET <kyradiscord@gmail.com>
2018-07-11 11:44:05 +0200

    fix: Resume command

--- src/commands/Music/resume.js
+++ src/commands/Music/resume.js
@@ -10,8 +10,8 @@ module.exports = class extends MusicCommand {
 	}
 
 	async run(msg) {
-		if (!msg.guild.music.paused) throw 'My deck is empty! Give me a disk first so I can lift the spirits in this room!';
-		if (msg.guild.music.playing) throw 'Is this song too silent, my friend? Because it is indeed... playing.';
+		if (!msg.guild.music.playing) throw 'My deck is empty! Give me a disk first so I can lift the spirits in this room!';
+		if (!msg.guild.music.paused) throw 'Is this song too silent, my friend? Because it is indeed... playing.';
 
 		msg.guild.music.resume();
 		return msg.sendMessage('▶ Resumed');
