6e80c4a3e11dfbdaae03bda803db959fd0a92fc7
kyranet <kyradiscord@gmail.com>
2018-11-18 15:25:45 +0100

    feat: SocketStatistics IPC Monitor

---
+++ src/ipcMonitors/socketStatistics.ts
@@ -0,0 +1,93 @@
+import { AeliaClient } from '../lib/AeliaClient';
+import { IPCMonitor } from '../lib/structures/IPCMonitor';
+
+export default class extends IPCMonitor {
+
+	public client: AeliaClient;
+
+	public async run(): Promise<StatisticsResults> {
+		const memoryUsage = process.memoryUsage();
+		return {
+			name: 'aelia',
+			presence: null,
+			statistics: this.client.ws.shards.map((shard) => ({
+				heapTotal: memoryUsage.heapTotal,
+				heapUsed: memoryUsage.heapUsed,
+				ping: shard.pings,
+				status: shard.status
+			}))
+		};
+	}
+
+}
+
+/**
+ * The return from the broadcast
+ */
+export type StatisticsResults = {
+	name: string;
+	presence: PresenceType | null;
+	statistics: ClientStatistics[];
+};
+
+/**
+ * The client statistics for each shard
+ */
+export type ClientStatistics = {
+	status: WebsocketStatus;
+	heapUsed: number;
+	heapTotal: number;
+	ping: number[];
+};
+
+/**
+ * The presence types
+ */
+export enum PresenceType {
+	/**
+	 * The Online status
+	 */
+	Online = 'online',
+	/**
+	 * The Offline status
+	 */
+	Offline = 'offline',
+	/**
+	 * The Idle status
+	 */
+	Idle = 'idle',
+	/**
+	 * The Do Not Disturb status
+	 */
+	DoNotDisturb = 'dnd'
+}
+
+/**
+ * The websocket status
+ */
+export enum WebsocketStatus {
+	/**
+	 * The websocket is active
+	 */
+	Ready,
+	/**
+	 * The websocket is connecting
+	 */
+	Connecting,
+	/**
+	 * The websocket is reconnecting
+	 */
+	Reconnecting,
+	/**
+	 * The websocket is idle
+	 */
+	Idle,
+	/**
+	 * The websocket is nearly ready, fetching last data
+	 */
+	Nearly,
+	/**
+	 * The websocket is disconnected
+	 */
+	Disconnected
+}
